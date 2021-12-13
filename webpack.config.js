7a144853c58299643e05a34a5973e3f070ff2a9c
Timothy Vogelsang <tvogelsang@igloosoftware.com>
2021-12-13 16:16:17 -0500

    Updates

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const remoteComponentConfig = require("./remote-component.config.js").resolve;
+
+const externals = Object.keys(remoteComponentConfig).reduce(
+  (obj, key) => ({ ...obj, [key]: key }),
+  {}
+);
+
+module.exports = {
+	entry: './src/index.js',
+	externals,
+	output: {
+		filename: 'bundle.js',
+		libraryTarget: 'commonjs'
+	},
+	module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
